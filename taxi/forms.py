91dfcec8237eadff659bd2791bbc1c36145cf209
Illia <ilaruslanovich7@gmail.com>
2024-06-12 19:28:11 +0200

    solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -16,8 +16,8 @@ class DriverCreationForm(UserCreationForm):
                  "First 3 characters must be uppercase letters. "
                  "Last 5 characters must be digits")
         if len(license_number) == 8 and (license_number[:3].isupper()
-                                         and license_number[:3].isalpha() and
-                                         license_number[3:].isdigit()):
+                                         and license_number[:3].isalpha()
+                                         and license_number[3:].isdigit()):
             return license_number
         raise forms.ValidationError(error)
 
@@ -32,9 +32,9 @@ class DriverLicenseUpdateForm(forms.ModelForm):
         error = ("Must be 8 characters. "
                  "First 3 characters must be uppercase letters. "
                  "Last 5 characters must be digits")
-        if len(license_number) == 8 and license_number[:3].isupper() \
-                and license_number[:3].isalpha() \
-                and license_number[3:].isdigit():
+        if (len(license_number) == 8 and (license_number[:3].isupper()
+                                          and license_number[:3].isalpha()
+                                          and license_number[3:].isdigit())):
             return license_number
         else:
             raise forms.ValidationError(error)
